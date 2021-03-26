4809aa56b7c093ec0b419ab932d205328f15b3ad
elenakout <elenakout@gmail.com>
2021-03-26 16:03:22 +0200

    Check user role in the controller #61

--- register_user.php
+++ register_user.php
@@ -1,1 +1,24 @@
-<h1>register user controller</h1>
+<?php
+
+  session_start();
+
+  // Αρχικοποίηση μεταβλητών
+  $name = $role = $userId = $avatart = '';
+
+  // Ανάθεση μεταβλητών αν ο χρήστης είναι συνδεμενος
+  if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    $name = $_SESSION["name"];
+    $role = $_SESSION['role'];
+    $avatar = $_SESSION['avatar'];
+
+    if($role != 'admin') {
+      header("location: index.php");
+      exit;
+    }
+  }else {
+    header("location: index.php");
+    exit;
+  }
+
+
+  include('./view/form_register.php');
---
+++ view/form_register.php
@@ -0,0 +1,10 @@
+<?php
+  include('./view/header.php');
+  include('./view/navbar.php');
+?>
+
+<h1>Form register</h1>
+
+<?php
+  include('./view/footer.php');
+?>
