9b94a83740f71c117ca31e7fdfa857b1e784bc70
elenakout <elenakout@gmail.com>
2021-04-03 18:22:56 +0300

    form register and validation

--- view/form_register.php
+++ view/form_register.php
@@ -4,31 +4,61 @@
 ?>
 
 <main class="main">
-  <form name="register" action="./admin_users.php" method="post" class="form" onsubmit="return validateForm()">
-    <input type="hidden" name="action" value="submit">
-    <input type="hidden" name="roleuser" value="<?= $roleuser ?>">
-    <input type="text" name="name" placeholder="Όνομα">
-    <p class="error_name">Παρακαλώ πληκρολογήστε όνομα</p>
-    <input type="text" name="lastName" placeholder="Επίθετο">
-    <p class="error_lastname">Παρακαλώ πληκρολογήστε επίθετο</p>
-    <input type="text" name="regNum" placeholder="Αριθμός Μητρώου">
-    <p class="error_regnum">Παρακαλώ πληκρολογήστε αριθμό μητρώου</p>
-    <select name="gender" id="gender" class="form_input">
-      <option value="" disabled selected hidden>Παρακαλώ επιλέξτε φύλο</option>
-      <option value="male">Άρρεν</option>
-      <option value="female">Θήλυ</option>
-    </select>
-    <p class="error_gender">Παρακαλώ εισάγετε φύλο</p>
-    <?php if($roleuser === 'student') { ?>
-      <select name="semester" id="semester" class="form_input">
-        <option value="" disabled selected hidden>Παρακαλώ επιλέξτε εξάμηνο φοίτησης</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-      </select>
-    <?php } ?>
-    <input type="submit" value="υποβολη" class="btn"/>
-  </form>
+  <section class="dashboard">
+    <aside class="dashboard__aside">
+      <img src="./assets/images/<?= $avatar ?>" alt="<?= $lastname ?>" />
+      <div class="aside__info">
+        <p><?= $lastname ?></p>
+        <p><?= $name ?></p>
+        <?php if($role === 'admin') { ?>
+        <p><span>Γραμματεία</span></p>
+        <?php }else { ?>
+        <p><?= $role === 'teacher' ? 'Καθηγητής' : 'Μαθητης' ?> </p>
+        <?php } ?>
+      </div>
+      <a href="./dashboard_admin.php" class="dashboard__link "><img
+          src="./assets/icons/user-avatar-filled.svg" alt="avatar icon"> Μαθητές</a>
+      <a href="./dashboard_admin.php?page=teacher" class="dashboard__link"><img
+          src="./assets/icons/teacher-ico.svg" alt="avatar icon">Καθηγητές</a>
+      <a href="./dashboard_admin.php?page=admin" class="dashboard__link"><img
+          src="./assets/icons/admin-ico.svg" alt="avatar icon">Γραμματεία</a>
+      <a href="./dashboard_admin.php?page=classes" class="dashboard__link link__active"><img
+          src="./assets/icons/book-ico-alt.svg" alt="avatar icon">Μαθήματα</a>
+      <a href="./admin_classes.php" class="dashboard__link link__active"><img
+          src="./assets/icons/insert-alt.svg" alt="avatar icon">Δημιουργία Μαθήματος</a>
+    </aside>
+    <div class="dashboard__form">
+      <form name="register" action="./admin_users.php" method="post" class="form"
+        onsubmit="return validateForm()">
+        <input type="hidden" name="action" value="submit">
+        <input type="hidden" name="roleuser" value="<?= $roleuser ?>">
+        <input type="text" name="name" placeholder="Όνομα">
+        <p class="error_name">Παρακαλώ πληκρολογήστε όνομα</p>
+        <input type="text" name="lastName" placeholder="Επίθετο">
+        <p class="error_lastname">Παρακαλώ πληκρολογήστε επίθετο</p>
+        <input type="text" name="regNum" placeholder="Αριθμός Μητρώου">
+        <p class="error_regnum">Παρακαλώ πληκρολογήστε αριθμό μητρώου</p>
+        <p class="error_regnum_length">Ο αριθμός μητρώου πρέπει να έχει 6 χαρακτήρες</p>
+        <p class="error_regnum_numbers">Ο αριθμός μητρώου πρέπει να είναι αριθμός</p>
+        <select name="gender" id="gender" class="form_input">
+          <option value="" disabled selected hidden>Παρακαλώ επιλέξτε φύλο</option>
+          <option value="male">Άρρεν</option>
+          <option value="female">Θήλυ</option>
+        </select>
+        <p class="error_gender">Παρακαλώ εισάγετε φύλο</p>
+        <?php if($roleuser === 'student') { ?>
+        <select name="semester" id="semester" class="form_input">
+          <option value="" disabled selected hidden>Παρακαλώ επιλέξτε εξάμηνο φοίτησης</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <p class="error_semester">Παρακαλώ εισάγετε εξάμηνο φοίτησης</p>
+        <?php } ?>
+        <input type="submit" value="δημιουργια" class="btn" />
+      </form>
+    </div>
+  </section>
 </main>
 
 <?php
--- view/js/index.js
+++ view/js/index.js
@@ -10,6 +10,8 @@ function validateForm() {
   let name_error = document.querySelector('.error_name');
   let lastname_error = document.querySelector('.error_lastname');
   let regnum_error = document.querySelector('.error_regnum');
+  let regnum_length_error = document.querySelector('.error_regnum_length');
+  let regnum_numbers_error = document.querySelector('.error_regnum_numbers');
   let gender_error = document.querySelector('.error_gender');
   let semester_error = document.querySelector('.error_semester');
 
@@ -25,6 +27,7 @@ function validateForm() {
 
   if (lastname.value === '' || lastname.value === null) {
     lastname.style.borderBottom = "1px solid red";
+    lastname.focus();
     lastname_error.classList.add('show');
     return false;
   } else {
@@ -33,7 +36,9 @@ function validateForm() {
   }
 
   if (regnum.value === '' || regnum.value === null) {
+    console.log("length", regnum.value)
     regnum.style.borderBottom = "1px solid red";
+    regnum.focus();
     regnum_error.classList.add('show');
     return false;
   } else {
@@ -41,13 +46,44 @@ function validateForm() {
     regnum_error.classList.remove('show');
   }
 
+  if (regnum.value.length < 6) {
+    regnum.style.borderBottom = "1px solid red";
+    regnum.focus();
+    regnum_length_error.classList.add('show');
+    return false;
+  } else {
+    regnum.style.borderBottom = "1px solid var(--primary)";
+    regnum_length_error.classList.remove('show');
+  }
+
+  if (regnum.value.match(/^[0-9]+$/) != null) {
+    regnum.style.borderBottom = "1px solid red";
+    regnum.focus();
+    regnum_numbers_error.classList.add('show');
+    return false;
+  } else {
+    regnum.style.borderBottom = "1px solid var(--primary)";
+    regnum_numbers_error.classList.remove('show');
+  }
+
   if (gender.value === '') {
     gender.style.borderBottom = "1px solid red";
+    gender.focus();
     gender_error.classList.add('show');
     return false;
   } else {
     gender.style.borderBottom = "1px solid var(--primary)";
     gender_error.classList.remove('show');
+  }
+
+  if (semester.value === '') {
+    semester.style.borderBottom = "1px solid red";
+    semester.focus();
+    semester_error.classList.add('show');
+    return false;
+  } else {
+    semester.style.borderBottom = "1px solid var(--primary)";
+    semester_error.classList.remove('show');
   }
 
   return true;
