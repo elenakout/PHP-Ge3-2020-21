f506f30ad5f6673328ba9bd08ee562bc9c7beffe
elenakout <elenakout@gmail.com>
2021-04-06 19:54:43 +0300

    update comments

--- model/database.php
+++ model/database.php
@@ -1,8 +1,9 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=ge3-test';
+    $dsn = 'mysql:host=localhost;dbname=koutousi';
     $username = 'root';
 
-
+    // χρησιμοποιούμε τον βρόχο try/catch της php
+    // Εάν μέσα στο try-loop δημιουργηθεί κάποιο fatal error, τότε «πιάνει» το Exception (εδώ συγκεκριμένα είναι Exception του PDO) και το προσθέτει στην μεταβλητή $e και εκτελείται ο catch-βρόχος
     try {
         $db = new PDO($dsn, $username);
     } catch (PDOException $e) {
