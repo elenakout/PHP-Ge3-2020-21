086474672f180f8b03621392e141da5b24ddcf69
elenakout <elenakout@gmail.com>
2021-04-29 20:15:55 +0300

    feat: create dtd file

---
+++ admin_xml.php
@@ -0,0 +1,51 @@
+<?php
+require('./model/database.php');
+
+// Αρχικοποίηση μεταβλητών
+$name = $role = $avatar = $action =  '';
+
+// Ανάθεση μεταβλητών αν ο χρήστης είναι συνδεμενος
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+  $name = $_SESSION["name"];
+  $lastname = $_SESSION["lastName"];
+  $role = $_SESSION['role'];
+  $avatar = $_SESSION['avatar'];
+  // Έλαγχος αν ο χρήστης είναι admin
+  if ($role != 'admin') {
+    header("location: index.php");
+    exit;
+  }
+} else {
+  header("location: index.php");
+  exit;
+}
+
+// Δημιουργία μεταβλητών μέτα την υποβολή φόρμας από το χρήστη
+if ($_SERVER["REQUEST_METHOD"] == "POST") {};
+
+switch ($action) {
+  case 'create':
+    if($title && $description && $teacher && $semester){
+    $idclass = create_class($title, $description, $mandatory, $teacher, $semester);
+    $students = get_all_students();
+    foreach($students as $student){
+      add_students_to_class($student['ID'], $idclass, $teacher);
+    }
+    $_SESSION['msg'] = 'Το μάθημα δημιουργήθηκε με επιτυχία';
+    header("location: dashboard_admin.php?page=classes");
+  } else {
+    $error_message = 'Η δημιουργία του μαθήματος απέτυχε.';
+    $link = "admin_classes.php";
+    include('view/error.php');
+  }
+    break;
+  case '':
+    break;
+  default:
+    include('./view/form_create_class.php');
+    break;
+}
+
+// $error_message = 'Η επεξεργασία του μαθήματος απέτυχε.';
+// $link = "dashboard_admin.php?page=classes";
+// include('view/error.php');
